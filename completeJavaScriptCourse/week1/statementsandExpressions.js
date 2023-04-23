1d8b7b3b83cd9957687cace936d1393943ec249e
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-04-23 21:25:58 +0100

    Delete completeJavaScriptCourse directory

--- completeJavaScriptCourse/week1/statementsandExpressions.js
+++
@@ -1,6 +0,0 @@
-//An expression is a piece of code that produces a value
-// 3 + 4 will produce a value
-// 19 is an expression because it will prouce a value
-//true && false
-
-//A statement doesnt produce a value by itself
